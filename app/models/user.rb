0c59e39fe2e071ba25ffdb13b220866afa538a23
James Sherriff <jamessherriff158@gmail.com>
2016-07-12 15:15:37 +0100

    Properly working profile pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.profile_rul = auth.info.image
+      user.picture_url = auth.info.image
       user.save!
     end
   end
